ea4493c31e7f50ae3be71fa3b00da6d0cfb85262
Kartikey Tiwari <80645903+Kartikey-web@users.noreply.github.com>
2021-06-05 13:44:12 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> JavaScript and React.js</h1>
+      <p>
+        Hello my name is Kartikey Tiwari and this bootcamp that i've attended
+        recently was amazing . Our instructor was Mr.Shaurya Sir . We covered
+        everything from Scratch including JavaScript , React.js and HTML.
+      </p>
+    </div>
+  );
+}
+export default Note;
